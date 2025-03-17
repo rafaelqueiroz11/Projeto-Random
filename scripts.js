5f6e44921093c94b4705a5353adac58ca8f03e5c
rafaelqueiroz11 <rafinha.n.q@hotmail.com>
2025-03-17 14:07:08 -0300

    ajustes no javascript

--- scripts.js
+++ scripts.js
@@ -1,17 +1,13 @@
-function sortear(){
+function sortear() {
     let min = parseInt(document.querySelector(".input-min").value);
     let max = parseInt(document.querySelector(".input-max").value);
-
 
     if (isNaN(min) || isNaN(max) || min >= max) {
         alert("Por favor, insira valores válidos!");
         return;
     }
-    
-    
-    let numero = Math.floor(Math.random() * (max - min + 1)) + min;
-            document.getElementById("resultado").textContent = numero;
 
-    
+    let numero = Math.floor(Math.random() * (max - min + 1)) + min;
+    document.getElementById("resultado").textContent = numero;
 }
 
